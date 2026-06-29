211df127b15725f2502409d2cc46607a61ceb553
W <wagnerlc07@hotmail.com>
2026-06-29 01:07:05 +0100

    Portfolio: HTML/Js de Galeria incluídos

--- portfolio.js
+++ portfolio.js
@@ -12,3 +12,45 @@ function menuNav (){
         `;
     container.innerHTML = html;
 }
+
+//Galeria
+
+    const preview = document.getElementById('gallery-gatman'); //Exibição principal
+    const defaultPreview = preview.src; //Default da foto principal
+    const thumbs = Array.from(document.querySelectorAll('gallery-thumbs')); //Imagens
+
+    let resetTimer;
+    let currentIndex = -1;
+
+    //Reset imagem
+    function resetPreview(){
+        preview.classList.add('fade-out');
+
+        setTimeout(() => {
+            preview.src = defaultPreview;
+            preview.classList.remove('fade-out');
+            currentIndex = -1;
+            
+        }, 100);
+    }
+
+    //Timer
+    function restartPreviewTimer(){
+        clearTimeout(resetTimer);
+        resetTimer = setTimeout(resetPreview, 5000);
+    }
+
+    //Mostrar imagem
+    function showImage(index){
+        currentIndex = (index + thumbs.length) % thumbs.length;
+        preview.classList.add('fade-out');
+
+        setTimeout(() => {
+           preview.src = defaultPreview;
+           preview.classList.remove('fade-out'); 
+           currentIndex = -1;
+        
+        }, 100);
+
+        restartPreviewTimer();
+    }
